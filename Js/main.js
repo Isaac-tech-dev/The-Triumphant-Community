1d0603a8c717c8d7e5767cae382926625a5eaff7
Isaac Ayeni <ayeniisaac1on1@gmail.com>
2023-04-02 20:05:51 +0100

    Seventh Commit

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,20 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
